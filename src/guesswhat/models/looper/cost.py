85d147383792bbd7d6f9bf4fd1fd83a9663fd977
Ian Clotworthy <clotworthyi@gmail.com>
2022-10-12 00:50:34 +0300

    Update cost.py
    completed cost function and made it easier to understand when reading section 3.1

--- src/guesswhat/models/looper/cost.py
+++ src/guesswhat/models/looper/cost.py
@@ -5,7 +5,12 @@ def IOU_dist(q1,q2):
     sim = len(set(q1) & set(q2)) / len(set(q1).union(set(q2)))
     return 1 - sim
 
-
 def cost_fn(q1,q2,max_length,a,b):
-    exponent = (max_length/max_length-1)*(np.absolute(a-b))/(max_length-1)
-    return np.power(2,exponent) -1
+    TD = (max_length/max_length-1) - (np.absolute(a-b))/(max_length-1)
+    K_dist = np.power(2,TD) -1
+    K_rep = 1/IOU_dist(q1,q2)
+    return K_dist + K_rep
+
+#def cost_fn(q1,q2,max_length,a,b):
+#    exponent = (max_length/max_length-1)*(np.absolute(a-b))/(max_length-1)
+#    return np.power(2,exponent) -1
